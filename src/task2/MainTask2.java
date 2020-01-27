74cb9ab57a586cd2c2934e072d0a9ccd2a27ae46
fkulesz <fkulesz@poczta.onet.pl>
2020-01-27 11:30:57 +0100

    task 2 uzupełnianka

---
+++ src/task2/MainTask2.java
@@ -0,0 +1,13 @@
+package task2;
+
+public class MainTask2 {
+    public static void main(String[] args) {
+        System.out.println("Klasa to definicja obiektu");
+        System.out.println("Obiekt to  instancja klasy");
+        System.out.println("Każda zmienna musi mieć określony typ, np. int, albo String");
+        System.out.println("Jeżeli deklarację zmiennej poprzedzimy słowem final, to znaczy że ta zmienna jest niezmienialna ");
+        System.out.println("W klasie mogą się znajdować zmienne, metody i obiekty ą wywoływane przez operator kropki i służą do tworzenia nowych obiektów. ");
+        System.out.println("Jeśli w klasie zdefiniowano chociaż jeden konstruktor to kompilator nie wygeneruje dla nas obiektu domyślnego.");
+        System.out.println("car1 i car2 to dwie osobne instancje wskazujące na ten sam obiekt");
+    }
+}
